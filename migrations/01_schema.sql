0deeb1977e2a268d20e87f17d92befba16750b37
Rosemary Ku <rosemary.ku@hotmail.com>
2022-02-08 22:13:51 -0500

    add 02_seeds

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -13,7 +13,6 @@ CREATE TABLE users (
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-
   title VARCHAR(255) NOT NULL,
   description TEXT,
   thumbnail_photo_url VARCHAR(255) NOT NULL,
@@ -22,13 +21,11 @@ CREATE TABLE properties (
   parking_spaces INTEGER  NOT NULL DEFAULT 0,
   number_of_bathrooms INTEGER  NOT NULL DEFAULT 0,
   number_of_bedrooms INTEGER  NOT NULL DEFAULT 0,
-
   country VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
   city VARCHAR(255) NOT NULL,
   province VARCHAR(255) NOT NULL,
   post_code VARCHAR(255) NOT NULL,
-
   active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
