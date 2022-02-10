ddc45733830641f58597e10dbe5ab9148687e05b
Rosemary Ku <rosemary.ku@hotmail.com>
2022-02-10 12:32:55 -0500

    change add property function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -128,14 +128,13 @@ const getAllProperties = function (options, limit = 10) {
     queryString += `HAVING AVG(property_reviews.rating) >= $${queryParams.length}`;
   }
   
-
   queryParams.push(limit);
   queryString += `
   ORDER BY cost_per_night
   LIMIT $${queryParams.length};
   `;
 
-  console.log('Query String: ', queryString, 'QueryParams: ', queryParams);
+  // console.log(queryString, queryParams);
 
   return pool.query(queryString, queryParams).then((res) => res.rows);
 };
@@ -150,9 +149,16 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+
+  const queryParams = [`${property.owner_id}`,`${property.title}`,`${property.description}`,`${property.thumbnail_photo_url}`,`${property.cover_photo_url}`,`${property.cost_per_night}`,`${property.street}`,`${property.city}`,`${property.province}`,`${property.post_code}`,`${property.country}`,`${property.parking_spaces}`,`${property.number_of_bathrooms}`, `${property.number_of_bedrooms}`]
+
+  const queryString = `INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;`
+
+  return pool
+    .query(queryString, queryParams)
+    .then(res => res.rows[0])
+    .catch(err => err.message);
+
 }
 exports.addProperty = addProperty;
